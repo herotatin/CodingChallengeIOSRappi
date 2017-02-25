5d1fc16efcd0a1dd95d40ffd8ed6feccc923bd40
Jose Omar Colorado Perez <jose.tatin1825@gmail.com>
2017-02-25 02:12:19 -0500

    Versión inicial del proyecto, donde se crea la estructura de trabajo bajo el patrón MVC

---
+++ CodingChallenge/CodingChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodingChallenge
+//
+//  Created by Jose Omar Colorado Perez on 25/02/17.
+//  Copyright © 2017 Jose Omar Colorado Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
